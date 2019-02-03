1d93cae3b848c0a3036096961b424f17405497ca
rjforty <raaj.ahab@gmail.com>
2019-02-03 12:54:13 +0000

    Revert "Removed momentjs from project and added xdate"
    This reverts commit 71cd40fe8391222aa3c27526058708ba92187487.

--- screens/Bible/components/BibleBar.js
+++ screens/Bible/components/BibleBar.js
@@ -23,34 +23,26 @@ export default class extends Component{
     render() {
         return (
             <View style={[styles.container]}>
-                <View style={{ flexDirection: "row", justifyContent: "space-between", width: "100%" }}>
-                    <TouchableOpacity onPress={() => this.props.navigateToBibleBookScreen()}>
-                        <View style={[styles.barItem]}>
-                            <StyledTextInverse style={[styles.barItemText]}>{this.getCurrentBook()}</StyledTextInverse>
-                            <EvilIcons name="chevron-down" style={{ alignSelf: "center"}} size={25} color="#FFFFFF"/>
-                        </View>
-                    </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.props.navigateToBibleBookScreen()}>
+                    <View style={[styles.barItem]}>
+                        <StyledTextInverse style={[styles.barItemText]}>{this.getCurrentBook()}</StyledTextInverse>
+                        <EvilIcons name="chevron-down" style={{ alignSelf: "center"}} size={25} color="#FFFFFF"/>
+                    </View>
+                </TouchableOpacity>
 
-                    <TouchableOpacity onPress={() => this.props.navigateToBibleChapterScreen()}>
-                        <View style={[styles.barItem]}>
-                            <StyledTextInverse style={[styles.barItemText]}>{this.getCurrentChapter()}</StyledTextInverse>
-                            <EvilIcons name="chevron-down" style={{ alignSelf: "center"}} size={25} color="#FFFFFF"/>
-                        </View>
-                    </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.props.navigateToBibleChapterScreen()}>
+                    <View style={[styles.barItem]}>
+                        <StyledTextInverse style={[styles.barItemText]}>{this.getCurrentChapter()}</StyledTextInverse>
+                        <EvilIcons name="chevron-down" style={{ alignSelf: "center"}} size={25} color="#FFFFFF"/>
+                    </View>
+                </TouchableOpacity>
 
-                    <TouchableOpacity>
-                        <View style={[styles.barItem]}>
-                            <StyledTextInverse style={[styles.barItemText]}>KJV</StyledTextInverse>
-                            <EvilIcons name="chevron-down" style={{ alignSelf: "center"}} size={25} color="#FFFFFF"/>
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity>
-                        <View style={[styles.barItem]}>
-                            <EvilIcons name="eye" style={{ alignSelf: "center"}} size={25} color="#FFFFFF"/>
-                        </View>
-                    </TouchableOpacity>
-                </View>
+                <TouchableOpacity>
+                    <View style={[styles.barItem]}>
+                        <StyledTextInverse style={[styles.barItemText]}>KJV</StyledTextInverse>
+                        <EvilIcons name="chevron-down" style={{ alignSelf: "center"}} size={25} color="#FFFFFF"/>
+                    </View>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -61,14 +53,15 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: "#387ecb",
         height: 80,
-        paddingHorizontal: 30,
-        paddingVertical: 10,
         justifyContent: "center",
         alignItems: "flex-end",
         flexDirection: "row"
     },
 
     barItem: {
+        margin: 10,
+        marginLeft: 20,
+        marginRight: 20,
         flexDirection: "row"
     },
 
