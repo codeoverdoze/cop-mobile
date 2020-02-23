230d73697089ab58b91248f6dde9c380014db759
Raaj <raaj.ahab@gmail.com>
2020-02-23 14:40:35 +0000

    fix(header): Fixes edge case in child header when header is longer than the container of child header

--- components/ChildScreenHeader.js
+++ components/ChildScreenHeader.js
@@ -1,12 +1,12 @@
-import React from "react";
-import styled from "styled-components";
-import propTypes from "prop-types";
-import { withNavigation } from "react-navigation";
+import React from 'react';
+import styled from 'styled-components';
+import propTypes from 'prop-types';
+import { withNavigation } from 'react-navigation';
 
 // components
-import SVGIcon from "./SVGIcon";
-import { StyledTextInverse } from "./Typography";
-import { backIcon } from "../assets/icons";
+import SVGIcon from './SVGIcon';
+import { StyledTextInverse } from './Typography';
+import { backIcon } from '../assets/icons';
 
 function ChildScreenHeader({ title, navigation }) {
   return (
@@ -15,15 +15,19 @@ function ChildScreenHeader({ title, navigation }) {
         <SVGIcon source={backIcon} fill="#ffffff" width={25} />
       </BackButton>
 
-      <Title>
-        <StyledTextInverse style={{ fontSize: 20 }}>{title}</StyledTextInverse>
-      </Title>
+      <TitleContainer>
+        <Title>
+          <StyledTextInverse style={{ fontSize: 20, textAlign: 'center' }} numberOfLines={1}>
+            {title}
+          </StyledTextInverse>
+        </Title>
+      </TitleContainer>
     </Container>
   );
 }
 
 ChildScreenHeader.propTypes = {
-  title: propTypes.string.isRequired
+  title: propTypes.string.isRequired,
 };
 
 const Container = styled.View`
@@ -40,10 +44,14 @@ const BackButton = styled.TouchableOpacity`
   top: 20%;
 `;
 
-const Title = styled.View`
-  width: 100%;
-  align-items: center;
+const TitleContainer = styled.View`
   justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.View`
+  width: 60%;
 `;
 
 export default withNavigation(ChildScreenHeader);
